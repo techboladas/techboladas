d00faccd34732b7a51bc4a1f76d6967e44e9b636
techboladas <techboladas@gmail.com>
2023-12-08 00:02:50 +0200

    fix: email link bug

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -9,7 +9,7 @@ export const Contact = () => {
         <h2 className="text-xl text-pallete-white mb-5">Contacte-nos</h2>
         <article className="">
           <LinkBlock Icon={FaWhatsapp} href="https://wa.me/message/HIOFD5VOTKMRF1">Whatsapp</LinkBlock>
-          <LinkBlock Icon={MdAlternateEmail} href="techboladas@gmail.com">Email</LinkBlock>
+          <LinkBlock Icon={MdAlternateEmail} href="mailto:techboladas@gmail.com">Email</LinkBlock>
           <LinkBlock Icon={FaInstagram} href="https://instagram.com/tech_boladas?igshid=NGVhN2U2NjQ0Yg==">Instagram</LinkBlock>
           <LinkBlock Icon={FaFacebook} href="https://www.facebook.com/profile.php?id=61553282626827&mibextid=3JLxgoXK8XkhKqoE">Facebook</LinkBlock>        
         </article>
