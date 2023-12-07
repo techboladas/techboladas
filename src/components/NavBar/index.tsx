bcdc0aa7fff8ec6b4302c949c7cad39ef3583250
techboladas <techboladas@gmail.com>
2023-12-07 14:27:59 +0200

    add: menu items

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -2,10 +2,10 @@
 
 import { FaWhatsapp, FaFacebook, FaInstagram, FaGithub } from 'react-icons/fa'
 import { IoIosMenu } from 'react-icons/io'
-import Link from "next/link"
 import { useMenu } from "./hook/useMenu"
 import { Menu } from './components/Menu'
 import { MenuItem } from './components/MenuItem'
+import { Link } from 'react-scroll'
 
 export const NavBar = () => {
   const { handleClick, grid, width, contentDisplay: display } = useMenu();
@@ -13,9 +13,10 @@ export const NavBar = () => {
   return (
     <nav className={`${width} h-screen bg-tech-secondary  p-1 text-2xl text-white fixed  grid ${grid} auto-rows-max  transition-all`}>
       <section className={`${display} flex-col gap-10 text-center w-48 mt-28  justify-self-end  col-span-2`}>
-        <Link href="#">Apresentacao</Link>
-        <Link href="#">Servicos</Link>
-        <Link href="#">Contacto</Link>
+        <Link to="apresentation" smooth duration={500}>Apresentacao</Link>
+        <Link to="service" smooth duration={500}>Servicos</Link>
+        <Link to="catalog" smooth duration={500}>Catalogo</Link>
+        <Link to="contact"smooth duration={500}>Contacto</Link>
         <p className="h-full self-end ">Create by <strong>T</strong></p>
       </section>
       <Menu className="col-span-1">
@@ -23,22 +24,22 @@ export const NavBar = () => {
            <IoIosMenu style={{ fontSize: '60px'}}  /> 
           </MenuItem>
           <MenuItem>
-            <Link href="https://github.com/techboladas/" target="_blank" >
+            <Link to="https://github.com/techboladas/" target="_blank" >
               <FaGithub style={ICON_STYLE} />
             </Link>
           </MenuItem>
           <MenuItem>
-            <Link href="https://facebook.com/techboladas/" target="_blank">
+            <Link to="https://facebook.com/techboladas/" target="_blank">
             <FaFacebook style={ICON_STYLE} />
             </Link>
           </MenuItem>
           <MenuItem>
-            <Link href="@techboladas">
+            <Link to="@techboladas">
               <FaInstagram style={ICON_STYLE}/>
             </Link>
           </MenuItem>
           <MenuItem>
-             <Link href="https://whatshap.com/send=872204494">
+             <Link to="https://whatshap.com/send=872204494">
               <FaWhatsapp style={ICON_STYLE} />
             </Link>
           </MenuItem>
