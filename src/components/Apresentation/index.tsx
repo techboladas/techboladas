59a7feba522ef6459f10b477d411a7121757f6d6
techboladas <techboladas@gmail.com>
2024-01-19 23:42:12 +0200

    feat: refactore the nav and apresentation section

--- src/components/Apresentation/index.tsx
+++ src/components/Apresentation/index.tsx
@@ -1,20 +1,27 @@
 "use client";
 
+import { FaWhatsapp } from "react-icons/fa";
 import { Link } from "react-scroll";
 
 export const Apresentation = () => {
   return (
-    <section id="apresentation" className="flex justify-center">
-      <p className="w-4/5 mt-44 p-5 pl-10 grid text-2xl text-pallete-white md:w-1/2 ">
-        Ola nos somos a tech boladas uma empresa de prestaçao de servicos de
-        software.
+    <section
+      id="apresentation"
+      className="flex flex-col justify-center items-center"
+    >
+      <h1 className="text-3xl self-end text-white font-extrabold mt-44 mb-4 sm:self-center md:text-5xl">
+        Tech Boladas
+      </h1>
+      <p className="w-4/5  p-5 pl-10 grid text-2xl text-pallete-white md:w-1/2 ">
+        Ola nos somos uma empresa de prestaçao de servicos .
         <br />
         <Link
           to="contact"
           smooth
           duration={500}
-          className="bg-pallete-10 mt-5 w-max p-3 text-pallete-white text-xl rounded-full justify-self-end  pinter"
+          className="bg-pallete-10 mt-5 w-max p-3 text-pallete-60 text-xl font-bold rounded-full justify-self-end  pinter flex gap-4 items-center"
         >
+          <FaWhatsapp />
           Contacte-nos
         </Link>
       </p>
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,49 +1,67 @@
-"use client"
+"use client";
 
-import { FaWhatsapp, FaFacebook, FaInstagram, FaGithub } from 'react-icons/fa'
-import { IoIosMenu } from 'react-icons/io'
-import { useMenu } from "./hook/useMenu"
-import { Menu } from './components/Menu'
-import { MenuItem } from './components/MenuItem'
-import { Link } from 'react-scroll'
-import A from 'next/link'
+import { FaWhatsapp, FaFacebook, FaInstagram, FaGithub } from "react-icons/fa";
+import { IoIosMenu } from "react-icons/io";
+import { useMenu } from "./hook/useMenu";
+import { Menu } from "./components/Menu";
+import { MenuItem } from "./components/MenuItem";
+import { Link } from "react-scroll";
+import A from "next/link";
 
 export const NavBar = () => {
   const { handleClick, grid, width, contentDisplay: display } = useMenu();
-  const ICON_STYLE = { fontSize: '42px' }
+  const ICON_STYLE = { fontSize: "42px" };
   return (
-    <nav className={`${width} h-screen bg-tech-secondary  p-1 text-2xl text-white fixed  grid ${grid} auto-rows-max  transition-all`}>
-      <section className={`${display} flex-col gap-10 text-center w-48 mt-28  justify-self-end  col-span-2`}>
-        <Link to="apresentation" smooth duration={500} >Apresentacao</Link>
-        <Link to="service" smooth duration={500}>Servicos</Link>
-        <Link to="catalog" smooth duration={500}>Catalogo</Link>
-        <Link to="contact"smooth duration={500}>Contacto</Link>
+    <nav
+      className={`${width} h-screen bg-tech-secondary   text-2xl text-white fixed  grid ${grid} auto-rows-max  transition-all`}
+    >
+      <section
+        className={`${display} flex-col gap-10 text-center w-48 mt-28  justify-self-end  col-span-2`}
+      >
+        <Link to="apresentation" smooth duration={500}>
+          Apresentacao
+        </Link>
+        <Link to="service" smooth duration={500}>
+          Servicos
+        </Link>
+        <Link to="catalog" smooth duration={500}>
+          Catalogo
+        </Link>
+        <Link to="contact" smooth duration={500}>
+          Contacto
+        </Link>
       </section>
       <Menu className="col-span-1">
-          <MenuItem className=" self-start justify-self-center w-full h-full border-0" onClick={handleClick}>
-           <IoIosMenu style={{ fontSize: '60px'}}  /> 
-          </MenuItem>
-          <MenuItem>
-            <A href="https://github.com/techboladas/" target="_blank" >
-              <FaGithub style={ICON_STYLE} />
-            </A>
-          </MenuItem>
-          <MenuItem>
-            <A href="https://www.facebook.com/profile.php?id=61553282626827&mibextid=3JLxgoXK8XkhKqoE" target="_blank">
+        <MenuItem
+          className=" self-start justify-self-center w-full h-full border-none "
+          onClick={handleClick}
+        >
+          <IoIosMenu style={{ fontSize: "60px" }} />
+        </MenuItem>
+        <MenuItem>
+          <A href="https://github.com/techboladas/" target="_blank">
+            <FaGithub style={ICON_STYLE} />
+          </A>
+        </MenuItem>
+        <MenuItem>
+          <A
+            href="https://www.facebook.com/profile.php?id=61553282626827&mibextid=3JLxgoXK8XkhKqoE"
+            target="_blank"
+          >
             <FaFacebook style={ICON_STYLE} />
-            </A>
-          </MenuItem>
-          <MenuItem>
-            <A href="https://instagram.com/tech_boladas?igshid=NGVhN2U2NjQ0Yg==">
-              <FaInstagram style={ICON_STYLE}/>
-            </A>
-          </MenuItem>
-          <MenuItem>
-             <A href="https://wa.me/message/HIOFD5VOTKMRF1">
-              <FaWhatsapp style={ICON_STYLE} />
-            </A>
-          </MenuItem>
+          </A>
+        </MenuItem>
+        <MenuItem>
+          <A href="https://instagram.com/tech_boladas?igshid=NGVhN2U2NjQ0Yg==">
+            <FaInstagram style={ICON_STYLE} />
+          </A>
+        </MenuItem>
+        <MenuItem>
+          <A href="https://wa.me/message/HIOFD5VOTKMRF1">
+            <FaWhatsapp style={ICON_STYLE} />
+          </A>
+        </MenuItem>
       </Menu>
     </nav>
-  )
-}
+  );
+};
